8f5f5e4d6602c171a30693042fff3a57fff7ebf3
Amanda Gettys <afgettys@gmail.com>
2020-09-07 13:52:58 -0400

    initial files/folders

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span id ="footer">Made with ❤ in SLC </span>
+    </footer>
+  );
+}
+
+export default Footer;
