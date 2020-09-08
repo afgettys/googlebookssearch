80fc167407bbf0976bcb801bad8a97ddd9431121
Amanda Gettys <afgettys@gmail.com>
2020-09-07 21:39:13 -0400

    adding components

--- googlebooksearch/src/App.js
+++ googlebooksearch/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+//Pages 
+import About from "./pages/About/About";
+import Saved from "./pages/Saved/saved";
+import Search from "./pages/Search/search";
+//Components
+import Navbar from "./components/Navbar/navbar";
+import Footer from "./components/Footer/index"
+
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+    <Navbar/>
+    <Route exact path="/" component={About} />
+    <Route exact path="/saved" component={Saved} />
+    <Route exact path="/search" component={Search} />
     </div>
+    <Footer/>
+    </Router>
+
   );
 }
+
 
 export default App;
